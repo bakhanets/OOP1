7b2f1b0bf9efc2b3b0a4f24dae18953cdddeb04e
bakhanets <32462637+bakhanets@users.noreply.github.com>
2018-11-16 02:40:57 +0700

    Add files via upload

--- OOP1/src/Main.java
+++ OOP1/src/Main.java
@@ -4,29 +4,12 @@ public class Main {
 
     public static void main(String[] args) {
 
-        Vector first=new Vector(0);
-        System.identityHashCode(first);
+        Vector A = new Vector(3,-4,0);
 
-        //Vector second=new Vector();
-       // System.identityHashCode(second);
+        Vector B = new Vector(4, -4, 2);
 
-        Vector third=new Vector(2.5,3.2,4.2);
-        System.identityHashCode(third);
-
-
-        first.setX(10);
-
-        first.MultiplyByNumber(10);
-        first.Show();
-        System.out.println(first.Module());
-        first.Normalization();
-        first.Show();
-
-
-
-        /*System.out.println(first.hashCode());
-        System.out.println(second.hashCode());
-        System.out.print(third.hashCode());*/
+        System.out.println(A.cosBetween(A,B));
+        System.out.println(A.degreeBetween(A,B));
 
 
     }
--- OOP1/src/Vector.java
+++ OOP1/src/Vector.java
@@ -66,10 +66,50 @@ public class Vector {
         return Math.sqrt(X*X+Y*Y+Z*Z);
     }
 
+    public boolean checkСollinearity(Vector a,Vector b){ //Проверка векторов на коллинеарность
+        if ((a.X/b.X==a.Y/b.Y)&&(a.Y/b.Y==a.Z/b.Z)){
+            return true;
+        }
+        else return false;
+    }
+
     public void Normalization(){ //Нормирование вектора
         double k =Module();
         X/=k;
         Y/=k;
         Z/=k;
+    }
+    public Vector Sum(Vector a,Vector b){
+        Vector C=new Vector(a.X+b.X,a.Y+b.Y,a.Z+b.Z);
+        return C;
+    }
+    public Vector Difference(Vector a,Vector b){ // Есть другой вариант реализации
+        Vector C=new Vector(a.X-b.X,a.Y-b.Y,a.Z-b.Z);
+        return C;
+    }
+
+
+    public Vector Multiplication(Vector a,Vector b){ // Векторное произведение двух векторов
+        if (checkСollinearity(a,b)){ //Если векторы коллинеарны , то их произведение даёт нулевой вектор
+            return new Vector(0);
+        }
+        Vector C=new Vector(a.Y*b.Z-a.Z*b.Y,a.Z*b.X-a.X*b.Z,a.X*b.Y-a.Y*b.X);
+        return C;
+    }
+
+    public double scalarMultiplication(Vector a,Vector b){ // Скалярное произведение векторов
+        return a.X*b.X+a.Y*b.Y+a.Z*b.Z;
+    }
+
+    public double cosBetween(Vector a,Vector b){//Косинус угла между векторами
+        return (scalarMultiplication(a, b))/(a.Module()*b.Module());
+    }
+
+    public double sinBetween(Vector a,Vector b){
+        return 1-cosBetween(a,b);
+    }
+
+    public double degreeBetween(Vector a, Vector b){//находит угол между векторами значение в градусах
+        return Math.toDegrees(Math.acos(cosBetween(a,b)));
     }
 }
